274e71483b167ad4bab03815eeb378d0af006a7b
Matheus Fernandes Rabelo <104003912+suehtaMFr@users.noreply.github.com>
2023-01-19 13:50:01 -0300

    Update 10679.cpp

--- 10679.cpp
+++ 10679.cpp
@@ -1,26 +1,47 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main () {	
+vector<int> kmp(string s){
+	
+	int k=0,n=(int)s.size();
+	vector<int> p(n);
+	p[0]=0; 	
+	for(int i=1;i<n;i++)
+	{
+		while(k>0 && s[i]!=s[k]) k=p[k-1];
+		if(s[k]==s[i]) k++;
+		p[i]=k;
+	}
+	return p;
+}
+
+bool match(string t,string s,vector<int> p) {
+	
+	vector<int> r;
+	int k=0,szt=(int)t.size(),szs=(int)s.size();
+	for(int i=0;i<szt;i++)
+	{
+		while(k>0 && t[i]!=s[k]) k=p[k-1];
+		if(t[i]==s[k]) k++;
+		if(k==szs) return true;
+	}
+	return false;
+}
+
+int main () {
 	
 	int tc;
 	cin >> tc;
-	for(int i=0;i<tc;i++)
+	while(tc--)
 	{
-		string a,c;
-		int b;
-		cin >> a >> b;
-		for(int j=0;j<b;j++)
+		int q;
+		string s,x;
+		cin >> s >> q;
+		for(int i=0;i<q;i++)
 		{
-			bool ans=false;
-			cin >> c;
-			for(int k=0,l=0;k<(int)a.size()&&!ans;k++)
-			{
-				l=(a[k]==c[l] ? l+1:0);
-				ans=(l==(int)c.size());
-			}
-			cout << (ans ? 'y':'n') << '\n';
+			cin >> x;
+			cout << (match(s,x,kmp(x)) ? 'y':'n') << "\n";
 		}
 	}
 }
