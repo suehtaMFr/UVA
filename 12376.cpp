2d531cd7c76d5b5e0c80e5c38b2b10d27175fcd4
Matheus Fernandes Rabelo <104003912+suehtaMFr@users.noreply.github.com>
2023-02-27 16:29:55 -0300

    Update 12376.cpp

--- 12376.cpp
+++ 12376.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 #define f first
 #define s second
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-void bfs(int& o,int& q,vector<priority_queue<pair<int,int>>> adj){
+void dfs(int& o,int& q,vector<priority_queue<pair<int,int>>> adj){
     
     if(adj[o].size())
     {
         q=q+adj[o].top().f;
         o=adj[o].top().s;
-        bfs(o,q,adj);
+        dfs(o,q,adj);
     }
 }
 
 int main(){
    
+   fast;
    int ct;
    cin >> ct;
    for(int i=0;i<ct;i++)
@@ -28,16 +28,14 @@ int main(){
        int p[v];
        vector<priority_queue<pair<int,int>>> adj(v);
        for(int j=0;j<v;j++)
-       {
            cin >> p[j];
-       }
        for(int j=0;j<e;j++)
        {
            cin >> v1 >> v2;
            adj[v1].push({p[v2],v2});
        }
        int o=0,q=0;
-       bfs(o,q,adj);
+       dfs(o,q,adj);
        cout <<"Case " << i+1 << ": " <<q << " " << o << "\n";
    }
 }
