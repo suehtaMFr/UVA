9bf4e901c573524f0d85efdd8f19e8c092296d3b
Matheus Fernandes Rabelo <104003912+suehtaMFr@users.noreply.github.com>
2023-04-19 20:05:43 -0300

    Create 686.cpp

---
+++ 686.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+//Regular show
+#define sz(x) ((int)x.size())
+#define fin freopen ("a.txt","r",stdin);
+#define fout freopen ("b.txt","w",stdout);
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+//rapadura é doce, mas não é mole não
+#define ff first
+#define ss second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+//I see you
+std::mt19937 rng((int) std::chrono ::steady_clock::now().time_since_epoch().count());
+std::mt19937_64 rng64((int) std::chrono::steady_clock::now().time_since_epoch().count());
+//~ long double pi = 3.141592653589793;
+
+int main () {
+	
+	fast;
+	vector<int> v;
+	bool prime[50001] = {0};
+	set<int> st;
+	for(int i = 2; i <= 50000;i++){
+		if(!prime[i]){
+			v.pb(i);
+			st.insert(i);
+			for(int j = i + i ;j <= 50000;j += i)
+				prime[j] = true;
+		}
+	}
+	int n;
+	while(cin >> n && n){
+		int s = 0, p = 0, q = upper_bound(all(v),n) - v.begin();
+		q = (q>=sz(v) ? sz(v) : q);
+		set<int> f;
+	    while(q--){
+	    	p = (st.count(n - v[q]) ? n - v[q] : 0);
+            s+= (p && (!f.count(p) || !f.count(v[q])) && p+v[q]==n);
+	        if(p) { f.insert(p);f.insert(v[q]);}
+	    }
+	    cout << s << "\n";
+	}
+}
