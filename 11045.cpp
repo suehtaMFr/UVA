d1b3f9f0425c30d6e343ef4b7f1cbf198b4a1fbc
Matheus Fernandes Rabelo <104003912+suehtaMFr@users.noreply.github.com>
2023-05-15 08:14:42 -0300

    11045.cpp

--- 11045.cpp
+++ 11045.cpp
@@ -1,137 +1,96 @@
-#include <iostream>
-#include <cstring>
-#include <vector>
-#include <queue>
-#include <map>
-
-#define pb push_back
-
+#include <bits/stdc++.h>
+ 
 using namespace std;
+ 
+//Regular show
+#define sz(x) ((int)x.size())
+#define fin freopen ("input.txt","r",stdin);
+#define fout freopen ("output.txt","w",stdout);
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+//rapadura é doce, mas não é mole não
+#define ff first
+#define ss second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+//I see you
+std::mt19937 rng((int) std::chrono ::steady_clock::now().time_since_epoch().count());
+std::mt19937_64 rng64((int) std::chrono::steady_clock::now().time_since_epoch().count());
 
-int INF = 999999999;
 
-int capacity[61][61];
-vector<int> adj[61];
+/*
+t | out =  0
+XXL     =  1
+XL      =  2
+L       =  3
+M       =  4
+S       =  5
+XS      =  6
+s | in  =  7
+*/
 
-map<string,int> tra;
-
-void ini(int n){
-	
-	adj[54].pb(60);//l
-	adj[55].pb(60);//xl
-	adj[56].pb(60);//xxl
-	adj[57].pb(60);//sx
-	adj[58].pb(60);//m
-	adj[59].pb(60);//s
-
-	adj[60].pb(54);
-	adj[60].pb(55);
-	adj[60].pb(56);
-	adj[60].pb(57);
-	adj[60].pb(58);
-	adj[60].pb(59);
-	
-	tra["L"]=54;
-	tra["XL"]=55;
-	tra["XXL"]=56;
-	tra["XS"]=57;
-	tra["M"]=58;
-	tra["S"]=59;
-	
-	capacity[54][60]=(n/6);
-	capacity[55][60]=(n/6);
-	capacity[56][60]=(n/6);
-	capacity[57][60]=(n/6);
-	capacity[58][60]=(n/6);
-	capacity[59][60]=(n/6);
-}
+int n;
+vector<vector<int>> adj(40), cp(40,vector<int> (40));// cp = capacity
 
 int bfs(int s, int t, vector<int>& parent) {
-    fill(parent.begin(), parent.end(), -1);
-    parent[s] = -2;
-    queue<pair<int, int>> q;
-    q.push({s, INF});
-
-    while (!q.empty()) {
-        int cur = q.front().first;
-        int flow = q.front().second;
-        q.pop();
-
-        for (int next : adj[cur]) {
-            if (parent[next] == -1 && capacity[cur][next]) {
-                parent[next] = cur;
-                int new_flow = min(flow, capacity[cur][next]);
-                if (next == t)
-                    return new_flow;
-                q.push({next, new_flow});
-            }
-        }
-    }
-
-    return 0;
+	fill(parent.begin(), parent.end(), -1);
+	parent[s] = -2;
+	queue<pair<int, int>> q;
+	q.push({s, INT_MAX});
+	while (!q.empty()) {
+		int cur = q.front().first;
+		int flow = q.front().second;
+		q.pop();
+		for (int next : adj[cur]) {
+			if (parent[next] == -1 && cp[cur][next]) {
+				parent[next] = cur;
+				int new_flow = min(flow, cp[cur][next]);
+				if (next == t) return new_flow;
+				q.push({next, new_flow});
+			}
+		}
+	}
+	return 0;
 }
 
 int maxflow(int s, int t) {
     int flow = 0;
-    vector<int> parent(61);
+    vector<int> parent(40);
     int new_flow;
-
     while (new_flow = bfs(s, t, parent)) {
         flow += new_flow;
         int cur = t;
         while (cur != s) {
             int prev = parent[cur];
-            capacity[prev][cur] -= new_flow;
-            capacity[cur][prev] += new_flow;
+            cp[prev][cur] -= new_flow;
+            cp[cur][prev] += new_flow;
             cur = prev;
         }
     }
-
     return flow;
 }
 
+inline int posi(string s){ return (s == "XXL" ? 1 : (s == "XL" ? 2 : (s == "L" ? 3 : (s == "M" ? 4 : (s == "S" ? 5 : 6)))));}
+
 int main () {
 	
-	
-	int l,nn,m;
-	cin >> l;
-	
-	for(int z=0;z<l;z++)
-	{
-		
-		cin >> nn  >> m;
-		string st1,st2;
-		ini(nn);
-		for(int y=1;y<=m;y++)
-		{
-			cin >> st1 >> st2;
-			
-			adj[y].pb(tra[st1]);
-			adj[tra[st1]].pb(y);
-			adj[y].pb(tra[st2]);
-			adj[tra[st2]].pb(y);
-			adj[0].pb(y);
-			adj[y].pb(0);
-			
-			capacity[y][tra[st1]]=1;
-			capacity[y][tra[st2]]=1;
-			capacity[0][y]=1;
+	fast;
+	for(int i = 1; i <= 6; i++) adj[i].pb(0);
+	int tc,n,m; cin >> tc;
+	string l, r;
+	while(tc--){
+		cin >> n >> m;
+		cp = vector<vector<int>>(40, vector<int>(40, 0));
+		for(int i = 1; i <= 6; i++) cp[i][0] = n/6;
+		for(int i = 8, x, y; i < m+8; i++){
+			cin >> l >> r;
+			x = posi(l), y = posi(r); 
+			adj[7].pb(i);
+			cp[7][i] = 1;
+			adj[i].pb(x); adj[i].pb(y); adj[x].pb(i); adj[y].pb(i);
+			cp[i][x] = cp[i][y] = 1; cp[x][i] = cp [y][i] = 1;
 		}
-
-		int res = maxflow(0,60);
-		if(m==res)
-		{
-			cout << "YES\n";
-		}
-		else
-		{
-			cout << "NO\n";
-		}
-		for(int it=0;it<61;it++)
-		{
-			adj[it].clear();
-		}
-		memset(adj,0,sizeof(adj));
+		cout << (maxflow(7,0)>=m ? "YES\n" : "NO\n");
+		for(int i = 7;i<40;i++) adj[i].clear();
 	}
-	
 }
