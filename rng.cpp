afd1c6798b9745a779c5f26b876bcdb006d02552
Matheus Fernandes Rabelo <104003912+suehtaMFr@users.noreply.github.com>
2023-08-15 16:13:24 -0300

    rng.cpp

---
+++ rng.cpp
@@ -0,0 +1,3 @@
+//apenas o suco
+std::mt19937 rng((int) std::chrono ::steady_clock::now().time_since_epoch().count());
+std::mt19937_64 rng64((int) std::chrono::steady_clock::now().time_since_epoch().count());
