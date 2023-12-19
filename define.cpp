509756de35a2b821b1a93913880a7c25d44d6e41
Matheus Fernandes Rabelo <104003912+suehtaMFr@users.noreply.github.com>
2023-12-19 08:07:17 -0300

    define.cpp

--- define.cpp
+++ define.cpp
@@ -2,27 +2,25 @@
  
 using namespace std;
  
-//Regular show
+// Regular show
 #define import python
+#define mid ((l+r)>>1)
 #define sz(x) ((int)x.size())
-#define fin freopen ("input.txt","r",stdin);
-#define fout freopen ("output.txt","w",stdout);
+#define fin freopen ("in.txt","r",stdin);
+#define fout freopen ("out.txt","w",stdout);
 #define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
-//rapadura é doce, mas não é mole não
+// Rapadura eh doce, mas nao eh mole nao.
 #define ff first
 #define ss second
 #define pb push_back
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
-//I see you
+// I see you
 typedef long long int ll;
-// long double pi = 3.141592653589793;
+typedef unsigned long long int ull;
 // priority_queue<int,vector<int>,greater<int>> pq;
 
-int main () {
-	
-	fast;
-	
+int main (){
 	
 	
 	
